b1a97e1e55b976318805829fed86ceff527c9a35
pzillas-design <pzillas@gmail.com>
2025-12-11 16:31:00 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gmrt.de',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.gmrt.de',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 

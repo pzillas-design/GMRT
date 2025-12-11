41454ab2eed24b96e7050cce270e733061d69356
pzillas-design <pzillas@gmail.com>
2025-12-11 16:35:53 +0100

    fix: correct import path for getDictionary in privacy page

--- src/app/[lang]/privacy/page.tsx
+++ src/app/[lang]/privacy/page.tsx
@@ -1,5 +1,5 @@
 
-import { getDictionary } from '@/dictionaries';
+import { getDictionary } from '@/get-dictionary';
 import { SimpleHeader } from '@/components/headers/SimpleHeader';
 import { Container } from '@/components/ui/Container';
 
